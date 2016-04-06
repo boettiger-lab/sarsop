a607bc0481ce8d1682b6841f634d3950667a0e7f
Jeroen Ooms <jeroenooms@gmail.com>
2016-04-07 00:05:14 +0200

    Comments

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,4 +1,4 @@
-# Build against mingw-w64 build of libsodium 1.0.3
+# Build against MS Visual Studio builds of APPL
 if(!file.exists("../windows/appl-0.96")){
   if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/appl/archive/v0.96.zip", "lib.zip", quiet = TRUE)
