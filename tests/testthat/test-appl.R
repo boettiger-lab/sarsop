0749379ec6ee86b9634a17645ec123e960a77385
Carl Boettiger <cboettig@gmail.com>
2016-05-29 22:54:59 +0000

    extend test suite

--- tests/testthat/test-appl.R
+++ tests/testthat/test-appl.R
@@ -4,11 +4,11 @@ testthat::context("appl")
 testthat::test_that("appl base functions generates a policy", {
 
 
-  setwd(tempdir())
   model <- system.file("models/example.pomdp",  package = "appl")
-  policy <- pomdpsol(model, timeout = 2, stdout = FALSE)
+  f <- tempfile()
+  policy <- pomdpsol(model, output = f, timeout = 2, precision = 10)
 
-  expect_true(file.exists(policy))
+  expect_true(file.exists(f))
 
   #expected <- system.file("models/output.policy",  package = "appl")
   #testthat::expect_output_file(print(readLines(policy)), expected, update = TRUE)
