9d86f1af43aa621df506b86e9243f523d1d44d49
Carl Boettiger <cboettig@gmail.com>
2016-08-12 19:10:49 +0000

    fix test

--- tests/testthat/test-sarsop.R
+++ tests/testthat/test-sarsop.R
@@ -12,5 +12,6 @@ sim <- sim_pomdp(transition, observation, reward, discount,
 
 ## Check logging works
 log <- tempdir()
+log_data <- data.frame(model = "ricker", r = 0.1, K = 20, C = NA, sigma_g = 0.1, sigma_m = 0.1)
 alpha <- sarsop(transition, observation, reward, discount, precision = 10,
-                log_dir = log, parameters = data.frame(model = "ricker", r = 0.1, K = 20, C = NA))
+                log_dir = log, log_data = log_data)
