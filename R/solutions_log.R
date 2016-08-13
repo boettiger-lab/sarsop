67142c9b362f9ec580f86769102935fd2a39bd30
Carl Boettiger <cboettig@gmail.com>
2016-08-13 07:07:18 +0000

    add function for reading from logs

--- R/solutions_log.R
+++ R/solutions_log.R
@@ -1,6 +1,7 @@
 
 #' @importFrom uuid UUIDgenerate
 #' @importFrom utils read.csv write.table
+# @importFrom readr read_csv write_csv
 solutions_log <- function(policyx, pomdpx = NULL, log_dir = ".",
                           metafile = paste0(log_dir, "/meta.csv"),
                           id = uuid::UUIDgenerate(), status,
@@ -12,14 +13,41 @@ solutions_log <- function(policyx, pomdpx = NULL, log_dir = ".",
     file.copy(pomdpx, paste0(log_dir, "/", id, ".pomdpx"))
 
   append <- file.exists(metafile)
-  utils::write.table(data.frame(id, status, n_states, n_obs, n_actions, discount, date = Sys.time(), log_data),
-              file = metafile,
-              append = append, sep = ", ", row.names = FALSE, col.names = !append)
-
+  log <- data.frame(id, status, n_states, n_obs, n_actions, discount, date = Sys.time(), log_data)
+  #readr::write_csv(log, metafile, append = append)
+  utils::write.table(log, metafile, append = append, col.names = !append, sep=",", quote = FALSE, row.names = FALSE)
 }
 
 # parameters = data.frame(model = NA, r = NA, K = NA, C = NA)
 
-read_log <- function(parameters, log_dir = ".", metafile = paste0(log_dir, "/meta.csv")){
+
+#' alphas_from_log
+#'
+#' @param parameters a data.frame with the desired parameter values as given in metafile
+#' @param log_dir path to log directory
+#' @param metafile path to metafile index, assumed to be meta.csv in log_dir
+#'
+#' @return a list of alpha vectors matching the query, and a data.frame with the rows of the matching metadata.
+#' @export
+#'
+#' @examples
+alphas_from_log <- function(parameters, log_dir = ".", metafile = paste0(log_dir, "/meta.csv")){
+  #meta <- readr::read_csv(metafile)
   meta <- utils::read.csv(metafile)
+  for(p in names(parameters)){
+    meta <- meta[meta[[p]] %in% parameters[[p]], ]
+    #meta <- meta[ grepl(parameters[[p]],meta[[p]]), ]
+  }
+
+  alphas <- lapply(1:dim(meta)[[1]], function(i){
+    id <- meta[i,"id"]
+    n_a <- meta[i,"n_actions"]
+    results <- read_policyx(file = paste0(log_dir, "/", id, ".policyx"))
+    alpha <- regularize_alpha(results$alpha, results$alpha_action, n_a)
+    alpha
+  })
+
+  list(alphas = alphas, meta = meta)
 }
+
+# parameters = data.frame(model = "ricker", r = 0.1)
