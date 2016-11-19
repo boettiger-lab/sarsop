a8ff02aaa4da0ffbac26077aa8cf21df31f2c0d2
Carl Boettiger <cboettig@gmail.com>
2016-11-19 22:12:36 +0000

    rescale test

--- tests/testthat/test-sarsop-long.R
+++ tests/testthat/test-sarsop-long.R
@@ -1,14 +1,14 @@
 
-states <- 0:100
+states <- seq(0,1, length=50)
 actions <- states
 obs <- states
 sigma_g <- 0.1
-sigma_m <- 0.1
+sigma_m <- 0.5
 reward_fn <- function(x,h) pmin(x,h) # - .001*h
 discount <- 0.95
 
 r <- 1
-K <- 15
+K <- 0.75
 f <- function(x, h){
   s <- pmax(x - h, 0)
   s * exp(r * (1 - s / K) )
@@ -17,6 +17,6 @@ f <- function(x, h){
 
 
 m <- fisheries_matrices(states, actions, obs, reward_fn, f, sigma_g, sigma_m, noise = "lognormal")
-alpha <- sarsop(m$transition, m$observation, m$reward, discount, precision = 10)
+alpha <- sarsop(m$transition, m$observation, m$reward, discount, precision = .1)
 df <- compute_policy(alpha, m$transition, m$observation, m$reward)
-#ggplot(df, aes(states[state], states[state] - actions[policy])) + geom_line() + geom_point()
+#' ggplot2::ggplot(df, ggplot2::aes(states[state], states[state] - actions[policy])) + ggplot2::geom_line() + ggplot2::geom_point()
