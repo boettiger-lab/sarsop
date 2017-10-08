040b925301bf6df6ddc6ed9ea4674e52a52fcd2c
Carl Boettiger <cboettig@gmail.com>
2017-10-07 22:00:30 -0700

    fix bug in hindcast simulate

--- R/hindcast_pomdp.R
+++ R/hindcast_pomdp.R
@@ -42,12 +42,4 @@ hindcast_pomdp <- function(transition, observation, reward, discount, obs, actio
   }
 
 
-  update_belief <- function(state_prior, transition, observation, z0, a0){
-    belief <-
-      vapply(1:length(state_prior), function(i){
-            state_prior %*% transition[, i, a0] * observation[i, z0, a0]
-      }, numeric(1))
-    belief / sum(belief)
-  }
-
 compare_pomdp <- hindcast_pomdp
--- R/sim_pomdp.R
+++ R/sim_pomdp.R
@@ -94,7 +94,7 @@ sim_pomdp <- function(transition, observation, reward, discount,
   update_belief <- function(state_prior, transition, observation, z0, a0){
     belief <-
       vapply(1:length(state_prior), function(i){
-            state_prior %*% transition[, i, a0] * observation[i, z0, a0]
+            state_prior %*% transition[i, , a0] * observation[i, z0, a0]
       }, numeric(1))
     belief / sum(belief)
   }
