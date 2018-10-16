1e63ccd3992f5de70b769ec0b4d0e69ccb8fd87d
Carl Boettiger <cboettig@gmail.com>
2018-10-15 21:03:08 -0700

    Introduce automatic caching ability in sarsop function (#25)
    * simple memoise for sarsop

--- tests/testthat/test-write_pomdpx.R
+++ tests/testthat/test-write_pomdpx.R
@@ -3,7 +3,8 @@ testthat::context("write_pomdpx")
 testthat::test_that("write_pomdpx writes an xml problem file", {
   source(system.file("examples/fisheries-ex.R", package = "sarsop"))
   f <- tempfile()
-  write_pomdpx(transition, observation, reward, discount, file = f, digits = 4, digits2 = 10)
+  write_pomdpx(transition, observation, reward, discount,
+               file = f, digits = 4, digits2 = 10)
   x <- xml2::read_xml(f)
   testthat::expect_is(x, "xml_document")
 
